8b020307e0f4485b1865f9541c7240e6e9cbdb0f
nijama <9n0101@gmail.com>
2023-05-02 22:20:22 -0500

    v 1.1 Added "UNOFFICIAL"

--- Remove-wpp-status-button/remove-wpp-status.js
+++ Remove-wpp-status-button/remove-wpp-status.js
@@ -1,34 +1,34 @@
 /**
- * Remove Whatsapp Web Status Button V-1.0
+ * Remove wpp Web Status Button V-1.1
 
-Have you ever notices how you can mute someone' Whatsapp statuses on the phone, but cannot do the same on the Web version?
+Have you ever notices how you can mute someone' wpp statuses on the phone, but cannot do the same on the Web version?
 
-This extension hides the Whatsapp Web Status Button to avoid being distracted by the small notification icon from the numerous statuses that are impossible to mute.
+This extension hides the wpp Web Status Button to avoid being distracted by the small notification icon from the numerous statuses that are impossible to mute.
 
 After install, reload the page to activate.
 
  */
 
-// Whatsapp web takes a long time to load,
-// therefore an observer is required to 
+// wpp web takes a long time to load,
+// therefore an observer is required to
 // wait for the Status button to appear.
 let observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
     // If no new nodes were added, return.
-    if (!mutation.addedNodes) return
+    if (!mutation.addedNodes) return;
     let statusButton = document.querySelector('div[title="Status"]');
-    // Else if now the Status Button exists, set it's display to none and finished the observer.
-    if (statusButton !=  null){
+    // Else if now the Status Button exists, set it's display to none and end the observer.
+    if (statusButton != null) {
       statusButton.style.display = 'none';
-      observer.disconnect()
+      observer.disconnect();
     }
-  })
-})
+  });
+});
 
-// Start observer
+// Start the observer
 observer.observe(document.body, {
-    childList: true,
-    subtree: true,
-    attributes: false,
-    characterData: false
-})
+  childList: true,
+  subtree: true,
+  attributes: false,
+  characterData: false,
+});
